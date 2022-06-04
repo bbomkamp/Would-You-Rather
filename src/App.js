8a863aac828c5fa9de76f6c58e446954bdd96b94
Brad Bomkamp <37642026+bbomkamp@users.noreply.github.com>
2022-06-04 00:01:16 -0500

    navbar ui progress

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './components/Home';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path = {'/'} component = {Home}/>
+      </Switch>
+    </Router>
   );
 }
 
